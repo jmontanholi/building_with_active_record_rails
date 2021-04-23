cb2e51e8c1f7d533d2a373b764175f44372175f8
jmontanholi <jonunes9326@hotmail.com.br>
2021-04-23 15:11:06 -0300

    Fix rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :body, :user_id, :post_id, presence: true
-  validates :body, length: {minimum: 5 }
+  validates :body, length: { minimum: 5 }
   validates :user_id, :post_id, numericality: { only_integer: true }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,4 @@ class Post < ApplicationRecord
   validates :title, length: { minimum: 8 }
   validates :user_id, numericality: { only_integer: true }
   validates :body, length: { minimum: 20 }
-
 end
